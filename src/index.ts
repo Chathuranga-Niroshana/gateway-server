169d2025b0885a31fdc51f9b1c875593e35d62e4
Chathuranga Niroshana <134888303+Chathuranga-Niroshana@users.noreply.github.com>
2025-05-28 10:26:39 +0530

    Add helmet and morgan middleware for enhanced security and logging

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import './config/prisma'
-import { PORT } from "./config/env";
 import cors from 'cors'
+import morgan from "morgan";
+import helmet from "helmet";
 import companyRouter from './routes/companyRoutes'
 import authRouter from './routes/authRoutes'
 import userRoutes from './routes/userRoutes'
+import { PORT } from "./config/env";
 
 const app = express();
 app.use(express.json());
 app.use(cors())
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get('/', (req: Request, res: Response) => {
     res.send("Hello World");
@@ -17,6 +21,21 @@ app.get('/', (req: Request, res: Response) => {
 app.use('/api/company/', companyRouter)
 app.use('/api/auth/', authRouter)
 app.use('/api/user/', userRoutes)
+
+app.use((req: Request, res: Response) => {
+    res.status(404).json({ error: "Route Not Found" });
+});
+
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error("Error:", err.stack);
+    res.status(500).json({ error: "Internal Server Error" });
+});
+
+process.on("SIGINT", () => {
+    console.log("Server shutting down...");
+    process.exit();
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`)
